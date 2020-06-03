c45f5ec64e14aa7c50a326c6a20de6354de40931
Francis Tsang <frtsang40@gmail.com>
2020-06-03 11:12:38 -0700

    user can see single events

--- app.js
+++ app.js
@@ -1,20 +1,24 @@
 // INITIALIZE BODY-PARSER AND ADD IT TO APP
 const bodyParser = require('body-parser');
-
+const handlebars = require('handlebars');
+const { allowInsecurePrototypeAccess } = require('@handlebars/allow-prototype-access');
 // Initialize express
 const express = require('express')
 const app = express()
-
+const exphbs = require('express-handlebars')
+const hbs = exphbs.create({
+    defaultLayout: 'main',
+    handlebars: allowInsecurePrototypeAccess(handlebars),
+});
 // The following line must appear AFTER const app = express() and before your routes!
 app.use(bodyParser.urlencoded({ extended: true }));
 
 const models = require('./db/models');
 
-// require handlebars
-var exphbs = require('express-handlebars');
-
+// // require handlebars
+// var exphbs = require('express-handlebars');
 // Use "main" as our default layout
-app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.engine('handlebars', hbs.engine);
 // Use handlebars to render
 app.set('view engine', 'handlebars');
 
@@ -35,19 +39,35 @@ app.get('/', (req, res) => {
     })
 })
 
+
 // CREATE
 app.post('/events', (req, res) => {
     models.Event.create(req.body).then(event => {
-      res.redirect(`/`);
+      // Redirect to events/:id
+      res.redirect(`/events/${event.id}`)
+  
     }).catch((err) => {
       console.log(err)
     });
 })
 
 
+
 // NEW
 app.get('/events/new', (req, res) => {
     res.render('events-new', {});
+})
+
+// SHOW
+app.get('/events/:id', (req, res) => {
+    // Search for the event by its id that was passed in via req.params
+    models.Event.findByPk(req.params.id).then((event) => {
+      // If the id is for a valid event, show it
+      res.render('events-show', { event: event })
+    }).catch((err) => {
+      // if they id was for an event not in our db, log an error
+      console.log(err.message);
+    })
 })
 
 // Choose a port to listen on
